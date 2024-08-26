803097e664f8aba8702438ba188df7756c611758
Glorytefe <glorytefe@gmail.com>
2024-08-26 21:42:13 +0100

    task 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,5 +1,5 @@
--- creates an index idx_name_first_score on the table
--- names and the first letter of name and score
+-- creates an index idx_name_first_score on the table names
+-- with the first letter of name and score
 
 CREATE INDEX idx_name_first_score
 ON names(name(1));
